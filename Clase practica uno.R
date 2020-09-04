70182a7c7794466d58e4b6bcfd208754ccd5a239
CarlosGDiez <70762561+CarlosGDiez@users.noreply.github.com>
2020-09-04 09:21:24 -0500

    Create Clase practica uno.R

---
+++ Clase practica uno.R
@@ -0,0 +1,10 @@
+library(rio)
+#dos formas de abrir bases de datos, importando links y...subiendo la base de datos que les interese
+data= import("X")
+#todo se guarada en cloud no más.
+#se peude traer por ejemplo el potente mapa de gishap  que funciona en base a su poderoso repositorio en github de data libre 8actualziada siempre a un día de retrazo).
+data = import ("X")
+view(data)
+#Todo esta en github, donde ss ecomparten bases de datos y proyectos.
+#elt rabajo final será un repositorio en github
+#el github de escritorio solo para pulear lo virtual a la computadora para trabajrlo en rstudio. Puente entre github y rstudio. EN todos lasdos puedes editar, pero github no es un lugar para anlisis es uno de alamacenar, no puedes corre ahí cófdigos.
